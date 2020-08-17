2e1fab875d67bc379020e2c385fabcd7c3b2f2f0
Tommy Alhamra <tommy.alhamra@gmail.com>
2020-08-17 23:01:45 +0700

    update from linux

---
+++ 04-Sandhika Galih/book.js
@@ -0,0 +1,16 @@
+
+## Ganti Atribut Js
+===================================================
+
+let ganti = document.quearySelector("#tujuan");
+ganti.setAttribute('src', "link tujuannya");
+
+===================================================
+
+##Tipe Web Storage
+if (typeof(Storage) !== "undefined") {
+  // Browser mendukung sessionStorage/localStorage.
+} else {
+  // Browser tidak mendukung sessionStorage/LocalStorage
+}
+===================================================

c148ffd3d6daad529b48fae47ad479409cf0754e
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-08-04 19:33:12 +0700

    add default parameter

--- 05-Sekolah_Koding/es6/src/index.js
+++ 05-Sekolah_Koding/es6/src/index.js
@@ -1,5 +1,21 @@
-class Test {
+// * Ini adalah Default Parameter
 
+generateTitle = () => {
+    return "ini parameter"
 }
 
-console.log(Test)
+
+contoh1 = (title, tag = 'default') => {
+    console.log(title + ' punya tag ' + tag)
+}
+contoh2 = (title, tag) => {
+    tag = tag || 'default'
+    console.log(title + ' punya tag ' + tag)
+}
+contoh3 = (title = generateTitle(), tag = "default") => {
+    console.log(title + ' punya tag ' + tag)
+}
+
+contoh1("Belajar", "HTML")
+contoh2("Belajar", "HTML")
+contoh3()

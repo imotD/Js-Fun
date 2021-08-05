e18a04c9f5258151a0ad055a5baa85f273fd77b5
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-08-05 08:23:37 +0700

    add webpack

--- 05-Sekolah_Koding/es6/src/index.js
+++ 05-Sekolah_Koding/es6/src/index.js
@@ -1,9 +1,2 @@
-// * Ini adalah Spread dan Rest
-
-signIn = (username, password, umur) => {
-    console.log(username + ' password ' + password + ' umur ' + umur)
-}
-
-let data = ['imotd', '123', '25']
-
-signIn(...data)
+const names = 'imotD'
+console.log(names)
---
+++ 05-Sekolah_Koding/es6/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  // mode: 'production',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};

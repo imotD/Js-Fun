d172262be83df23e2735617a854c7bb87810502e
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-10-10 21:29:41 +0700

    add arrow function

---
+++ 04-Sandhika_Galih/oop/04_latihan_arrowFunction.js
@@ -0,0 +1,41 @@
+// * normalnya function declaration
+
+// let Mahasiswa = function (nama) {
+//     return `Perkenalkan nama saya ${nama}`
+// }
+
+// console.log(Mahasiswa('Tommy'))
+
+// * arrow function 
+
+// let Mahasiswa = (nama) => {
+//     return `Perkenalkan nama saya ${nama}`
+// }
+
+// console.log(Mahasiswa('Tommy'))
+
+
+// * implisit retun
+// jika parameternya hanya 1 tidak perlu menggunakan tanda kurung dan return
+
+
+// let Mahasiswa = nama =>
+//     `Perkenalkan nama saya ${nama}`
+
+
+// console.log(Mahasiswa('Tommy'))
+
+// *tanpa parameter 
+
+// let Mahasiswa = () =>
+//     `Perkenalkan nama saya`
+
+// console.log(Mahasiswa())
+
+
+
+
+
+// ! arrow
+// arrow fungsion tidak mengenal konsep THIS 
+// dan tidak semua fungsi declaration bisa diubah menjadi arrow fungsion

de1d806fca1d602b4e65469734cb070321307792
Tommy Alhamra <tommy@modoc.id>
2022-10-09 22:28:04 +0700

    Learn OOP by Programer Zaman Now

---
+++ 09-Programer_Zaman_Now/oop/constructor_function.js
@@ -0,0 +1,16 @@
+function Person(firstname, lastname) {
+  this.firstname = firstname
+  this.lastname = lastname
+  this.sayHello = function (name) {
+    console.info(`Hello ${name}, my name is ${this.firstname}`)
+  }
+}
+
+const eko = new Person('Eko', 'Setiadi')
+eko.sayHello('Bambang')
+
+const budi = new Person('Budi', 'Prakoso')
+budi.lastname = 'Angkasa'
+
+console.log(eko)
+console.log(budi)
---
+++ 09-Programer_Zaman_Now/oop/constructor_inheritance.js
@@ -0,0 +1,15 @@
+function Employe(firstname) {
+  this.firstname = firstname
+  this.sayHello = function (name) {
+    console.info(`Hello ${name}, my name is ${this.firstname}`)
+  }
+}
+
+function Manager(firstname, lastname) {
+  this.lastname = lastname
+  Employe.call(this, firstname)
+}
+
+const eko = new Manager('Eko', 'Bilar')
+
+console.log(eko)
